1396d2d1e506c929b5b6ba892be497f78e8ac83c
DarlingUUi <Dragon.dev1994@gmail.com>
2022-05-11 10:40:35 +0900

    third deploy

--- src/component/Footer/index.jsx
+++ src/component/Footer/index.jsx
@@ -1,21 +1,26 @@
 import style from './Footer.module.scss';
 
 const Footer = () => {
+    const urls = [
+        "https://www.otsuka.com/jp/",
+        "http://www.omd.otsuka.com/",
+        "https://otsuka.disclosure.site/ja"
+    ]
     return (
         <div className={style.footer}>
             <ul className="links js-footer-links">
                 <li>
-                    <a href="https://www.otsuka.com/jp/" target="_blank">
+                    <a href={urls[0]}>
                         <span>Otsuka Holdings Co.,ltd.</span>
                     </a>
                 </li>
                 <li>
-                    <a href="http://www.omd.otsuka.com/" target="_blank">
+                    <a href={urls[1]}>
                         <span>Otsuka Medical Devices Co.,ltd.</span>
                     </a>
                 </li>
                 <li>
-                    <a href="https://otsuka.disclosure.site/ja" target="_blank">
+                    <a href={urls[2]}>
                         <span>Otsuka Group Consolidated CSR</span>
                     </a>
                 </li>
